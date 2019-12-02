39ff040a6f2acff274e981eba16dc6e05868e61a
asia111307 <joapal@st.amu.edu.pl>
2019-12-02 16:12:27 +0100

    IT version launched

--- src/app/questions_it.ts
+++ src/app/questions_it.ts
@@ -1,13 +1,38 @@
 export const QUESTIONS_IT = [
-    ['Rwola "policji mózgowej" w ośrodkowych ukwładzie nerwowym należy do:', ['oligodendrocytów', 'astrocytów', 'mikrogleju', 'komórek ependymy'], 'c'],
-    ['Rola "policji mózgowej" w ośrowdkowych ukwładzie nerwowym należy do:', ['oligodendrocytów', 'astrocytów', 'mikrogleju', 'komórek ependymy'], 'c'],
-    ['Rola "policji mózgowej" w ośrodkowych układzie nerwowym należy do:', ['oligodendrocytów', 'astrocytów', 'mikrogleju', 'komórek ependymy'], 'c'],
-    ['Rola "powlicji mózgowej" w ośrodkowywch układzie nerwowym należy do:', ['oligodendrocytów', 'astrocytów', 'mikrogleju', 'komórek ependymy'], 'c'],
-    ['Rola "policji mówzwgowej" w ośrodkowych układzie nerwowym należy do:', ['oligodendrocytów', 'astrocytów', 'mikrogleju', 'komórek ependymy'], 'c'],
-    ['Rola "policji mwózgowej" w ośrodkowych układzie nerwowym należy do:', ['oligodendrocytów', 'astrocytów', 'mikrogleju', 'komórek ependymy'], 'c'],
-    ['Rola "policji mózgowej" w owśrodkowych układzie nerwowym należy do:', ['oligodendrocytów', 'astrocytów', 'mikrogleju', 'komórek ependymy'], 'c'],
-    ['Rola "policji mózgowej" w ośrodkowych ukławdzie nerwowym należy do:', ['oligodendrocytów', 'astrocytów', 'mikrogleju', 'komórek ependymy'], 'c'],
-    ['Rola "policji mózgowej" w ośrodkowywch układzie nerwowym należy do:', ['oligodendrocytów', 'astrocytów', 'mikrogleju', 'komórek ependymy'], 'c'],
-    ['Rola "policji mózgowej" w ośrodkowych ukławdzie nerwowym należy do:', ['oligodendrocytów', 'astrocytów', 'mikrogleju', 'komórek ependymy'], 'c'],
-    ]
+    ['Formalna reprezentacja określonej treści, zakodowana, nadająca się do przechowywania, przesyłania i przetwarzania to:', ['wiedza', 'dana', 'informacja', 'jednostka operacyjna'], 'b'],
+    ['Kto w swoim fundamentalnym dziele „Elementy” podał algorytm znajdowania największego wspólnego dzielnika dwóch liczb całkowitych dodatnich?', ['Euklides', 'Arystoteles', 'Pascal', 'Dekker'], 'a'],
+    ['Słowo "algorytm" wzięło się od perskiego matematka, którego nazwisko pisane po łacinie brzmi Algorismus. Podał on reguły na:', ['dodawanie, odejmowanie, mnożenie i dzielenie liczb dziesiętnych', 'obliczanie największego wspólnego dzielnika', 'obliczanie najmniejszej wspólnej wielokrotności', 'pierwiastkowanie i potęgowanie ułamków'], 'a'],
+    ['Kto zudował jedną z pierwszych maszyn wykonujących cztery działania oraz odkrył na nowo, pochodzący z Chin, system dwójkowy zapisu liczb?', ['Wilhelm Schickard', 'Ada Lovelace', 'Gottfried Wilhelm Leibnitz', 'Charles Babbage'], 'c'],
+    ['Za pierwowzór programu komputerowego uznaje się opis działania maszyny analitycznej Babbagea i zbiór uwag nt. metody obliczania liczb Bernoulliego za pomocą tej maszyny. Kto jest autorem tego opisu?', ['Charles Babbage', 'Herman Hollerith', 'Ada Lovelace', 'Wilhelm Schickard'], 'c'],
+    ['Ada Lovelace uważała, że maszyna analityczka Babbagea, która miała być zdolna do realizacji algorytmów, mogłaby:', ['obliczać wartości liczb zespolonych', 'sama tworzyć wydajniesze algorytmy', 'opracowywać dane statystyczne', 'tworzyć grafikę albo komponować muzykę'], 'd'],
+    ['Za stworzenie architektury współczesnego komputera jest odpowiedzialny:', ['Konrad Zuse', 'John von Neumann', 'Herman Hollerith', 'Ada Lovelace'], 'b'],
+    ['Zostały wynalezione w 1947 roku, a w 1955 zastąpiły lampy elektronowe​ - mowa tutaj o:', ['tranzystorach', 'układach scalonych wielkiej skali integracji​', 'procesorach Intel 4004', 'pierwszych kalkulatorach'], 'a'],
+    ['Kto był wynalazcą zera i zastowował pierwszy liczbowy system pozycyjny?', ['Rzymianie', 'Egipcjanie', 'Babilończycy', 'Grecy'], 'c'],
+    ['Heksadecymalny system liczbowy to inaczej system:', ['dwójkowy', 'szóstkowy', 'ósemkowy', 'szesnastkowy'], 'd'],
+    ['Do sposobów zapisu binarnego liczb całkowitych nie należy:', ['zapis znak-moduł (ZM)​', 'kod uzupełnieniowy do jedności (U1)', 'kod uzupełnieniowy do dwóch (U2)', 'kod uzupełnieniowy do czterech (U4)'], 'd'],
+    ['W zmiennopozycyjnym zapisie liczb rzeczywistych stosuje się mantysę znormalizowaną, która przyjmuje wartości z przedziału:', ['od -1 do 1', 'od -1 do 2', 'od 0 do 1', 'od 1 do 2'], 'd'],
+    ['Do cech dźwięku nie należy:', ['barwa​', 'głośność', 'gęstość', 'czas trwania'], 'c'],
+    ['Mieszanie się kolorów zgodnie z addytywną zasadą mieszania barw dotyczy modelu:', ['RGB', 'CMYK', 'SMYK', 'RBG'], 'a'],
+    ['Dźwięki o częstotliwości większej niż 10 GHz, obserwowane jedynie przy zjawisku akustooptycznym to:', ['milidźwięki', 'infradźwięki', 'ultradźwięki', 'hiperdźwięki'], 'd'],
+    ['Dźwięku o której z poniższych częstotliwości nie jest w stanie usłyszeć człowiek, ale jest w stanie usłyszeć pies?', ['18 Hz', '25 000 Hz', '32 000 Hz', 'nie istnieje taka częstotliwość'], 'b'],
+    ['Skrót określający liczbę nieruchomych klatek wyświetlanych w czasie jednej sekundy filmu​ to:', ['FPS', 'IFPS', 'CPS', 'BPS'], 'a'],
+    ['Rozdzielczość ekranu to:', ['liczba nieruchomych klatek wyświetlanych w czasie jednej sekundy filmu', 'liczba oddzielnych pikseli w każdym wymiarze, które mogą być wyświetlone​', 'standardowy sposób nadawania sygnału telewizyjneg', 'częstotliwość migotania obrazu telewizyjnego​'], 'b'],
+    ['Ojcem teorii informacji był:', ['Claude Shannon', 'Herman Hollerith', 'John von Neumann', 'Gottfried Wilhelm Leibnitz'], 'a'],
+    ['Teoria informacji jest działem:', ['biotechnologii', 'fizyki', 'cybernetyki', 'automatyki'], 'c'],
+    ['Jednostką informacji jest:', ['komunikat', 'entropia', 'bit', 'informacja nie jest właściwością posiadającą swoją jednostkę'], 'c'],
+    ['Zgodnie z teorią informacji, im większe prawdopodobieństwo wystąpienia danego komunikatu, tym:', ['mniej informacji ten komunikat zawiera', 'więcej informacji ten komunikat przenosi', 'mniejsze prawdopodobieństwo utracenia informacji', 'większe prawdopodobieństwo utracenia informacji'], 'a'],
+    ['Gdy źródło informacji nadaje tylko jeden komunikat o prawdopodobieństwie równym 1, to:', ['komunikat nie niesie żadnej informacji', 'komunikat niesie dokładnie jedną informację', 'komunikat niesie dokładnie dwie informacje', 'komunikat niesie nieskończenie wiele informacji'], 'a'],
+   ['Średnia ważona ilość informacji w komunikatach z jednego źródła to:', ['średnia długość słowa źródłowego', 'entropia informacyjna źródła', 'bit', 'redundancja kodu'], 'b'],
+   ['Kod zbioru komunikatów, w którym żaden ciąg kodowy nie jest początkiem innego ciągu oraz który ma minimalną redundancję, to kod:', ['zwarty', 'jednoznaczny', 'jednostkowy', 'jednoczęściowy'], 'a'],
+   ['Język definiowania danych, inaczej składnia poleceń pozwalających na tworzenie schematu bazy danych to:', ['DDL', 'DML', 'DCL', 'DQL'], 'a'],
+['Algorytm tłumaczenia danego kodu na kod o mniejszej redundancji nazywamy:', ['algorytmem kompresji stratnej', 'algorytmem kompresji bezstratnej', 'algorytmem kompresji redundancji', 'algorytmem redundancji'], 'b'],
+['Technika, w której dany kod zastąpić kodem, który jest podobny (według pewnej przyjętej miary) i oferuje znacznie niższą redundancję to:', ['kompresja stratnej', 'kompresja bezstratna', 'kompresja redundancji', 'algorytm redundancji'], 'a'],
+['Stosunek rozmiaru danych przed kompresją do rozmiaru danych skompresowanych to:', ['redundancja', 'entropia', 'stopień kompresji danych', 'kompresja stratna'], 'c'],
+    
+    
+
+
+]
 ;
+
+
